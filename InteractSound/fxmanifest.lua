67a39ed572f0002d3cc853e576741aa3b9f8cbc8
mixedsz <treygoodlik@gmail.com>
2026-04-27 03:02:44 +0100

    Add files via upload

---
+++ InteractSound/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+client_script 'client/main.lua'
+
+server_script 'server/main.lua'
+
+ui_page('client/html/index.html')
+
+files {
+    'client/html/index.html',
+    'client/html/sounds/demo.ogg',
+	'client/html/sounds/lockpick.ogg',
+	'client/html/sounds/seatbelt.ogg',
+	'client/html/sounds/seatbeltoff.ogg',
+	'client/html/sounds/unlock.ogg',
+	'client/html/sounds/off.ogg',
+	'client/html/sounds/on.ogg',
+	'client/html/sounds/hunger.ogg',
+	'client/html/sounds/lock.ogg',
+	'client/html/sounds/cell.ogg',
+	'client/html/sounds/carlock.ogg',
+	'client/html/sounds/shower.ogg',
+	'client/html/sounds/iphonetext.ogg'
+}
